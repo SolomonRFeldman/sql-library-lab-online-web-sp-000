95557ab1792aa6c75a18908d174e8aec1cdec989
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-06 12:12:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ def select_series_title_with_most_human_characters
   'SELECT series.title
   FROM series
   INNER JOIN books ON books.series_id = series.id
-  WHILE books.species = "human"'
+  '
 end
 
 def select_character_names_and_number_of_books_they_are_in
