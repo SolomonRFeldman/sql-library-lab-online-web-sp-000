682ed946bbab1f28503364d05dc5650394a88f58
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-06 12:16:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  'SELECT series.title
+  'SELECT series.title, COUNT(characters.species)
   FROM series
   INNER JOIN characters ON characters.series_id = series.id
   ORDER BY COUNT(characters.species)
